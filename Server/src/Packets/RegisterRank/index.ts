8004250aa31faa9b6458008c9fa1f834cdd959b2
Alex4386 <psh010209@gmail.com>
2019-06-20 19:06:29 +0900

    fix: make a snapshot instead of hot linking

--- Server/src/Packets/RegisterRank/index.ts
+++ Server/src/Packets/RegisterRank/index.ts
@@ -12,9 +12,16 @@ export interface RankFormat {
 
 export function registerRank(conn: WebSocket) {
     if (getUserIndexFromWebSocket(conn) !== -1) {
+        const a = currentUsers[getUserIndexFromWebSocket(conn)];
         ranks.push({
-            userName: currentUsers[getUserIndexFromWebSocket(conn)].userName,
-            gameInfo: currentUsers[getUserIndexFromWebSocket(conn)].gameInfo,
+            userName: a.userName,
+            gameInfo: {
+                score: a.gameInfo.score,
+                bombs: a.gameInfo.bombs,
+                power: a.gameInfo.power,
+                lifes: a.gameInfo.lifes,
+                difficulty: a.gameInfo.difficulty,
+            },
         });
         broadcastToListeners({ current: getSendSafeCurrentUsers(), ranks });
     }
